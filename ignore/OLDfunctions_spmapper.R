ae1a06d2fe6416167f74a1a72bb262e25bf25e5a
clacap-CEH <ccaporusso@gmx.com>
2025-06-16 13:55:20 +0100

    16/06/2025, CC: first draft of the package

---
+++ ignore/OLDfunctions_spmapper.R
@@ -0,0 +1,159 @@
+## ########################################
+#' @title TO BE ADDED - TITLE OF MAIN SPMAPPER FUNCTION
+#' @description TO BE ADDED - OVERALL DESCRIPTION OF WHAT SPMAPPER DOES
+#' @param spname Species name: a character string. Must be "Guillemot", "Kittiwake" or "Razorbill"
+#' @param fppolys Footprint(s): a simple feature collection
+#' @param popscen Population scenario: a character string. Must be "baseline", "low" or "high"
+#' @param tooldir Path to directory containing tool: a character string.
+#' @return A list, with elements: "spname" (species name), "popscen" (population scenario"), "energypars_base_est" (energetics parameters - estimates), "energypars_base_sim" (energetics parameters - simulations), "udmap" (utilisation distribution, a raster), "popsize" (population size - BREEDING PAIRS **TBC**, "fp_ud_overlap" (proportion of UD within a footprint), "nbirds_in_fp" (number of birds in footprints), "onebird_prey_est" (prey intake from one bird - estimate), "onebird_prey_sim" (prey intake from one bird - simulations), "allbirds_prey_est" (prey intake from all birds in footprints - estimate), "allbirds_prey_sim" (prey intake from all birds in footprints - simulations)
+#' @export
+spmapper <- function(spname, fppolys, popscen, tooldir){
+
+  ## ################################
+  ## 1. Get four letter species code
+
+  spcodes <- c("Kittiwake" = "BLKI", "Guillemot" = "COGU", "Razorbill" = "RAZO")
+
+  if(! any(spname == names(spcodes))){ stop("Invalid species name: must be 'Kittiwake', 'Guillemot' or 'Razorbill'!") }
+
+  spcode <- as.character(spcodes[which(names(spcodes) == spname)])
+
+  ## #################################
+  ## 2. Load population size data
+
+  if(! any(popscen == c("low", "baseline", "high"))){
+
+    stop("Invalid population scenario: must be 'low', 'baseline' or 'high'")
+  }
+
+  file_popsize <- system.file("extdata", "fame_population_ests.csv", package = "ecowings")
+
+  popsizes <- read.csv(file_popsize)
+
+  popsize <- popsizes$value[popsizes$spp == spcode & popsizes$estimate == popscen]
+
+  ## ################################
+  ## 3. Load data - energetics parameters - estimates
+
+  file_energypars_est <- system.file("extdata", "Seabird_energetics_pars_spmapper.csv", package = "ecowings")
+
+  energypars_base_est <- read.csv(file_energypars_est)
+
+  my_param <- energypars_base_est$parameter
+
+  ww <- which(colnames(energypars_base_est) == spname)
+
+  energypars_base_est <- data.frame(as.list(energypars_base_est[,ww]))
+
+  names(energypars_base_est) <- my_param
+
+  ## kable(t(energypars_base_est))
+
+  ## ################################
+  ## 4. Load dat - energetic parameters - simulations (capturing uncertainty)
+
+  file_energypars_sim <- system.file(
+    "extdata",
+    paste(spcode, "sampled", "params", "csv", sep = "."),
+    package = "ecowings"
+  )
+
+  energypars_base_sim <- read.csv(file_energypars_sim)
+
+  energypars_base_sim$init_mass_sd <- energypars_base_est$init_mass_sd ## fix missing values
+
+  energypars_base_sim$chicks_n[energypars_base_sim$chicks_n < 0.01] <- 0.01
+
+  ## kable(round(t(apply(energypars_base_sim, 2, summary)), 2))
+
+  ## ################################
+  ## 5. Load species-level UD
+
+  file_udmap <- system.file(
+    "extdata",
+    paste0("FAME_UD_standardised_", spcode, ".tif"),
+    package = "ecowings"
+  )
+
+  udmap <- terra::rast(file_udmap)
+
+  terra::crs(udmap) <- '+proj=laea +lat_0=-7.947 +lon_0=-14.30 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs +towgs84=0,0,0'
+
+  ## #################################
+  ## 6. Proportion of UD overlapping with polygons
+
+  fp_ud_overlap <- fpudoverlap(fppolys = fppolys, udmap = udmap)
+
+  ## #################################
+  ## 7. Number of birds in footprint
+
+  nbirds_in_fp <- popsize * fp_ud_overlap
+
+  ## #################################
+  ## 8. Prey calculation - estimates and simulations
+
+  onebird_prey_est <- fn.tot.prey(
+    TB_foraging = energypars_base_est$TB_foraging,
+    TB_flying = energypars_base_est$TB_flying,
+    TB_resting_sea = energypars_base_est$TB_resting_sea,
+    TB_nest = energypars_base_est$TB_nest,
+    ER_foraging = energypars_base_est$ER_foraging,
+    ER_flying = energypars_base_est$ER_flying,
+    ER_resting_sea = energypars_base_est$ER_resting_sea,
+    ER_nest = energypars_base_est$ER_nest,
+    ER_warming_food = energypars_base_est$ER_warming_food,
+    chick_ER = energypars_base_est$chick_ER,
+    chicks_n = energypars_base_est$chicks_n,
+    BS_length = energypars_base_est$BS_length,
+    init_mass = energypars_base_est$init_mass,
+    mass_change = energypars_base_est$mass_change,
+    tissue_en_dens = energypars_base_est$tissue_en_dens,
+    energy_prey_conv = energypars_base_est$energy_prey_conv,
+    assim_effic = energypars_base_est$assim_effic)
+
+  onebird_prey_sim <- fn.tot.prey(
+    TB_foraging = energypars_base_sim$TB_foraging,
+    TB_flying = energypars_base_sim$TB_flying,
+    TB_resting_sea = energypars_base_sim$TB_resting_sea,
+    TB_nest = energypars_base_sim$TB_nest,
+    ER_foraging = energypars_base_sim$ER_foraging,
+    ER_flying = energypars_base_sim$ER_flying,
+    ER_resting_sea = energypars_base_sim$ER_resting_sea,
+    ER_nest = energypars_base_sim$ER_nest,
+    ER_warming_food = energypars_base_sim$ER_warming_food,
+    chick_ER = energypars_base_sim$chick_ER,
+    chicks_n = energypars_base_sim$chicks_n,
+    BS_length = energypars_base_sim$BS_length,
+    init_mass = energypars_base_sim$init_mass,
+    mass_change = energypars_base_sim$mass_change,
+    tissue_en_dens = energypars_base_sim$tissue_en_dens,
+    energy_prey_conv = energypars_base_sim$energy_prey_conv,
+    assim_effic = energypars_base_sim$assim_effic)
+
+  ## #################################
+  ## 10. Total prey loss
+
+  allbirds_prey_est <- nbirds_in_fp * onebird_prey_est
+
+  allbirds_prey_sim <- nbirds_in_fp * onebird_prey_sim
+
+  ## #################################
+  ## 11. Output
+
+  list(
+    spname = spname,
+    popscen = popscen,
+    energypars_base_est = energypars_base_est,
+    energypars_base_sim = energypars_base_sim,
+    udmap = udmap,
+    popsize = popsize,
+    fp_ud_overlap = fp_ud_overlap,
+    nbirds_in_fp = nbirds_in_fp,
+    onebird_prey_est = onebird_prey_est,
+    onebird_prey_sim = onebird_prey_sim,
+    allbirds_prey_est = allbirds_prey_est,
+    allbirds_prey_sim = allbirds_prey_sim
+  )
+}
+
+## ########################################
---
+++ ignore/my_notes.R
@@ -0,0 +1,140 @@
+# guidance and notes on spmapper installation and running
+
+# all data is on datalabs
+# https://datalab.datalabs.ceh.ac.uk/resource/ecowings/spmapper/
+# spmapper
+# last extracted 16/06/2025, 11.30am
+
+#############################
+
+# run devtools & roxygen
+library(devtools)
+# devtools::install_github("klutometis/roxygen")
+library(roxygen2)
+
+# run this in the console with the path you have chosen it will create and open a new project, even if the
+# folder doesn't eist
+ create_package("N:/ecowings")
+# this spmapper folder is an R source package and a Rstudio project
+# we now make a git repository
+use_git()
+
+# add your functions to the R folder as .R files
+
+# document dependencies by adding required libraries in the DESCRIPTION file, e.g.
+# Imports:
+#sf,
+#terra
+
+# info
+# https://r-pkgs.org/whole-game.html
+# https://tinyheero.github.io/jekyll/update/2015/07/26/making-your-first-R-package.html
+
+# once the documentation is completed within each function file e.g.
+# @title @params, etc
+# run the below to generate .rd files for each function in the main folder
+# You will get one .Rd file for each function in your R package.
+devtools::document()
+
+# Making Raw Data Available within the package
+# Example data for example
+# load them in the folder inst/extdata
+# North sea polygon, UKSPA and UK_OWF_polygons_by_status in inst/demo as folders
+# make sure the names of the files don't have spaces or special chars
+# perhaps rename text files to ...._metadata
+# once the package is installed the data can be accessed with system.file
+# e.g. system.file("extdata", "fame_population_ests.csv", package = "spmapper")
+
+# Data should go in inst/extdata
+
+# testing package
+# usethis::use_testthat(3)
+#https://r-pkgs.org/testing-basics.html
+
+# you can create extra folder "demo" to store files used by vignette within "inst"
+
+# documentation goes in vignettes, same for spmapper-worked-example
+
+#######################################
+# installing the package
+#######################################
+# if we have
+#Our functions (.R files) the R folder
+#Documentation (.Rd) files in in the man folder
+#Data (binary and/or raw) in the data and inst/extdata
+# we should be good to go
+
+
+# make sure each R function file has export at the beggining. e.g.
+
+## ########################################
+#' @title TO BE ADDED - TITLE OF MAIN SPMAPPER FUNCTION
+#' @description TO BE ADDED - OVERALL DESCRIPTION OF WHAT SPMAPPER DOES
+#' @param spname Species name: a character string. Must be "Guillemot", "Kittiwake" or "Razorbill"
+#' @param fppolys Footprint(s): a simple feature collection
+#' @param popscen Population scenario: a character string. Must be "baseline", "low" or "high"
+#' @param tooldir Path to directory containing tool: a character string.
+#' @return A list, with elements: "spname" (species name), "popscen" (population scenario"), "energypars_base_est" (energetics parameters - estimates), "energypars_base_sim" (energetics parameters - simulations), "udmap" (utilisation distribution, a raster), "popsize" (population size - BREEDING PAIRS **TBC**, "fp_ud_overlap" (proportion of UD within a footprint), "nbirds_in_fp" (number of birds in footprints), "onebird_prey_est" (prey intake from one bird - estimate), "onebird_prey_sim" (prey intake from one bird - simulations), "allbirds_prey_est" (prey intake from all birds in footprints - estimate), "allbirds_prey_sim" (prey intake from all birds in footprints - simulations)
+#' @export
+
+# after changes
+devtools::build()
+# Creates a “tarball” (.tar.gz), which is the format used for sharing and submitting packages around like on CRAN.
+# But basically, when doing so, it ensures the package is structured correctly.
+
+
+# not sure if devtools::build() is interchangeable with the below as it also creates a vignette folder
+# but maybe not a template introduction.Rmd?
+# and vignette for creating page on github
+usethis::use_vignette("introduction")
+
+# if ready, install the package in your R system library
+devtools::install()
+# then load it
+library(spmapper)
+
+# render the vignettes
+# GitHub won’t directly render the .html file, but you can use the GitHub HTML Preview service.
+# Basically, you just give it the url of your html file from your GitHub and it will render it.
+# http://htmlpreview.github.io/
+
+# when testing introduction.Rmd choose knit to hmtl vignette NOT RUN
+
+# check that the function works by running ?spmapper for example
+?spmapper
+?fn.tot.prey
+spmapper()
+fn.tot.prey()
+spmapper::fn.tot.prey()
+
+# errors:
+
+#in functions_spmapper.R for the example data it leads you to "Data" but it should be inst/extdata
+
+# file_popsize <- file.path(tooldir, "Data", "fame_population_ests.csv")
+# repleace this (and similar files) with:
+# file_popsize <- system.file("extdata", "fame_population_ests.csv", package = "ecowings")
+
+# exception:
+file_energypars_sim <- file.path(tooldir, "Data", paste(spcode, "sampled", "params", "csv", sep="."))
+# to:
+file_energypars_sim <- system.file(
+  "extdata",
+  paste(spcode, "sampled", "params", "csv", sep = "."),
+  package = "mypkg"
+)
+
+# if pkg is corrupted unlink it
+unlink("C:/Users/clacap/AppData/Local/R/win-library/4.4/ecowings", recursive = TRUE, force = TRUE)
+# or manually remove it by browsing to it
+# clean
+devtools::clean_dll()
+# and try reinstalling
+
+# for visualising plots and charts use this:
+#```{r, fig.width=8, fig.height=8}
+
+
+
+# to push
+
